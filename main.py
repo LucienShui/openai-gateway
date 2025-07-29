f8b08775df5831004f10f9c0a57fab249216dbfa
Lucien <lucien@lucien.ink>
2025-07-29 16:44:05 +0800

    chore(log): Print log to console only by default

--- main.py
+++ main.py
@@ -205,7 +205,8 @@ def main():
         'main:app',
         host=os.getenv('HOST', '0.0.0.0'),
         port=int(os.getenv('PORT', '8000')),
-        workers=int(os.getenv('WORKERS', '1'))
+        workers=int(os.getenv('WORKERS', '1')),
+        log_level=os.getenv('LOG_LEVEL', 'critical'),
     )
 
 
