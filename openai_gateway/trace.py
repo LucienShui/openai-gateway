9a1c55407a0f900efaaf573c6a3859cb97aa5233
Lucien Shui <lucien@lucien.ink>
2025-08-22 09:26:37 +0800

    feat(trace): Support OpenTelemetry

---
+++ openai_gateway/trace.py
@@ -0,0 +1,28 @@
+import os
+
+from fastapi import FastAPI
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.sdk.resources import Resource, SERVICE_NAME, DEPLOYMENT_ENVIRONMENT
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+
+def patch_open_telemetry(target: FastAPI):
+    if (base_endpoint := os.environ.get("OTEL_EXPORTER_OTLP_ENDPOINT", None)) is None:
+        raise Exception("OTEL_EXPORTER_OTLP_ENDPOINT environment variable is not set")
+
+    resource = Resource.create(attributes={
+        SERVICE_NAME: "openai-gateway",
+        DEPLOYMENT_ENVIRONMENT: os.environ.get("ENV", "unknown")
+    })
+    trace_provider = TracerProvider(resource=resource)
+
+    endpoint = base_endpoint + "/v1/traces"
+    otlp_exporter = OTLPSpanExporter(endpoint=endpoint)
+    span_processor = BatchSpanProcessor(otlp_exporter)
+
+    trace_provider.add_span_processor(span_processor)
+    trace.set_tracer_provider(trace_provider)
+
+    FastAPIInstrumentor.instrument_app(target, excluded_urls="health", exclude_spans=["send"])
